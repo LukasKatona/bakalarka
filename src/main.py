50d59c615328f07219bffbfdc66cfafc9d2666a7
Lukas Katona <katona.lukasek@gmail.com>
2024-10-28 09:49:18 +0100

    feat: init

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from DEVS import Bus, BusLine, BusStop
+
+buss_stops = [
+    BusStop("Stop 1", 10, 1),
+    BusStop("Stop 2", 20, 1),
+    BusStop("Stop 3", 30, 1),
+    BusStop("Stop 4", 20, 1),
+    BusStop("Stop 5", 10, 1)
+]
+
+bus_line = BusLine(46, buss_stops, [5, 6, 4, 7])
+
+bus = Bus(5, bus_line)
+
+print("Simulation started")
+
+while bus.state != Bus.State.Finished:
+    if bus.state == Bus.State.Starting:
+        bus.start()
+    if bus.state == Bus.State.Traveling:
+        bus.travel_to_next_stop()
+    elif bus.state == Bus.State.Arrived:
+        bus.arrive_at_stop()
+    elif bus.state == Bus.State.Boarding:
+        bus.board_passengers()
+    elif bus.state == Bus.State.Departed:
+        bus.depart_stop()
+
+print("Simulation finished")
