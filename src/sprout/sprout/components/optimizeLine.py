260eddaa1745312b89b7a8d817c9916ba2d09de1
Lukas Katona <katona.lukasek@gmail.com>
2025-02-20 14:05:08 +0100

    feat: init optimize page

---
+++ src/sprout/sprout/components/optimizeLine.py
@@ -0,0 +1,4 @@
+import reflex as rx
+
+def optimizeLine() -> rx.Component:
+    return rx.text("Welcome to Optimization!!!"),
--- src/sprout/sprout/pages/optimizePage.py
+++ src/sprout/sprout/pages/optimizePage.py
@@ -1,6 +1,12 @@
 import reflex as rx
 
-from ..components.layout import layout
+from ..components.infoUpload import infoUpload
+from ..components.optimizeLine import optimizeLine
 
 def optimizePage() -> rx.Component:
-    return rx.text("Welcome to Optimization!!!"),
+    return rx.vstack(
+        infoUpload(),
+        optimizeLine(),
+        spacing="5",
+        width="100%",
+    )
