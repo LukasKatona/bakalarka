9c3d717cd3c722b527ff0a2d88efd9d6f232fcd0
Lukas Katona <katona.lukasek@gmail.com>
2025-04-28 16:06:11 +0200

    refactor: infoUpload change

--- src/sprout/sprout/components/infoUpload.py
+++ src/sprout/sprout/components/infoUpload.py
@@ -1,3 +1,7 @@
+"""
+This file contains info upload component and its state.
+"""
+
 import os
 import reflex as rx
 
@@ -10,12 +14,28 @@ from .optimizeLine import OptimizeLineState
 
 
 def getTextFromTXT(fileName: str) -> rx.Component:
+    """
+    This function opens the file and extracts the text inside.
+
+    :param fileName: Name of the given file
+    :type fileName: str
+    :return: Text from the file
+    :rtype: rx.Component
+    """
     file_path = os.path.join(os.path.dirname(__file__), fileName)
     with open(file_path, "r") as file:
         content = file.read()
     return rx.text(content, white_space="pre")
 
 def getFilePath(fileName: str) -> str:
+    """
+    Helper function for modifying the file name.
+
+    :param fileName: File name to modify
+    :type fileName: str
+    :return: Modified file name
+    :rtype: str
+    """
     return os.path.join(os.path.dirname(__file__), fileName)
 
 class InfoUploadState(rx.State):
@@ -24,6 +44,12 @@ class InfoUploadState(rx.State):
 
     @rx.event
     async def selectTimeTableFromDropdown(self, value: str): 
+        """
+        Selects timetable from dropdown.
+
+        :param value: Selected timetable name
+        :type value: str
+        """
         if self.router.page.path == "/analyze":
             stateClass = AnalyzeLineState
         if self.router.page.path == "/optimize":
@@ -35,10 +61,24 @@ class InfoUploadState(rx.State):
         state.timeTable = self.parseTimeTableToTuple(state.selectedTimeTable)
 
     def insertNewTimeTable(self, timeTable: tuple[str, str]):
+        """
+        Inserts new timetable to dropdown options
+
+        :param timeTable: Timetable to insert
+        :type timeTable: tuple[str, str]
+        """
         self.options.append(timeTable)
         self.dropdownOptions.append(timeTable[0])
 
     def parseBusStopsToTuple(self, selectedBusStops: str) -> list[tuple[str, str, bool]]:
+        """
+        Parses the text containing information about bus stops.
+
+        :param selectedBusStops: Bus stops information
+        :type selectedBusStops: str
+        :return: List of formated bus stops
+        :rtype: list[tuple[str, str, bool]]
+        """
         busStops = InputParser.parseBusStopsFromString(selectedBusStops)
         busStopTuple: list[tuple[str, str, bool]] = []
         even = True
@@ -48,6 +88,14 @@ class InfoUploadState(rx.State):
         return busStopTuple
 
     def parseTimeTableToTuple(self, selectedTimeTable: str) -> list[tuple[str, str, bool]]:
+        """
+        Parses the text containing niformation about timetable.
+
+        :param selectedTimeTable: Timetable information
+        :type selectedTimeTable: str
+        :return: Formated timetable
+        :rtype: list[tuple[str, str, bool]]
+        """
         timeTable = InputParser.parseTimeTableFromString(selectedTimeTable)
         timeTableTuple: list[tuple[str, str, bool]] = []
         for hour in range(24):
@@ -61,7 +109,13 @@ class InfoUploadState(rx.State):
         return timeTableTuple
 
     @rx.event
-    async def handle_upload_bus_stops(self, uploadBusStops: list[rx.UploadFile]):
+    async def handleUploadBusStops(self, uploadBusStops: list[rx.UploadFile]):
+        """
+        Uploads text file containing information about bus stops.
+
+        :param uploadBusStops: Bus stop file
+        :type uploadBusStops: list[rx.UploadFile]
+        """
         if self.router.page.path == "/analyze":
             stateClass = AnalyzeLineState
         if self.router.page.path == "/optimize":
@@ -74,7 +128,13 @@ class InfoUploadState(rx.State):
             state.initConstraints()
         
     @rx.event
-    async def handle_upload_time_table(self, uploadTimeTable: list[rx.UploadFile]):
+    async def handleUploadTimeTable(self, uploadTimeTable: list[rx.UploadFile]):
+        """
+        Uploads text file containing information about timetable.
+
+        :param uploadTimeTable: Timetable file
+        :type uploadTimeTable: list[rx.UploadFile]
+        """
         if self.router.page.path == "/analyze":
             stateClass = AnalyzeLineState
         if self.router.page.path == "/optimize":
@@ -86,6 +146,14 @@ class InfoUploadState(rx.State):
         state.timeTable = self.parseTimeTableToTuple(state.selectedTimeTable)
         
 def infoUpload(stateClass) -> rx.Component:
+    """
+    Info upload component, for uploading information about bus stops and timetable. Also displays selected bus stops and timetable.
+
+    :param stateClass: State of the page on which is the info upload component
+    :type stateClass: class
+    :return: Info upload component
+    :rtype: rx.Component
+    """
     return rx.vstack(
         rx.hstack(
             rx.vstack(
@@ -138,83 +206,85 @@ def infoUpload(stateClass) -> rx.Component:
                     multiple=False,
                     width="100%",
                     height="100%",
-                    on_drop=InfoUploadState.handle_upload_bus_stops(rx.upload_files("upload_bus_stops")),
+                    on_drop=InfoUploadState.handleUploadBusStops(rx.upload_files("upload_bus_stops")),
                 ),              
                 flex="1",
             ),
-            rx.vstack(
-                rx.hstack(
-                    rx.heading("Informácie o časovom rozpise linky"),
-                    rx.dialog.root(
-                        rx.dialog.trigger(rx.icon("info")),
-                        rx.dialog.content(
-                            rx.dialog.title("rozpis.txt"),
-                            rx.dialog.description(
-                                getTextFromTXT("rozpis.txt"),
-                            ),
-                            rx.flex(
-                                rx.dialog.close(
-                                    rx.button(
-                                        "Zatvoriť",
-                                        variant="soft",
-                                        color_scheme="gray",
-                                    ),
+            rx.cond(
+                stateClass == AnalyzeLineState,
+                rx.vstack(
+                    rx.hstack(
+                        rx.heading("Informácie o časovom rozpise linky"),
+                        rx.dialog.root(
+                            rx.dialog.trigger(rx.icon("info")),
+                            rx.dialog.content(
+                                rx.dialog.title("rozpis.txt"),
+                                rx.dialog.description(
+                                    getTextFromTXT("rozpis.txt"),
                                 ),
-                                rx.dialog.close(
-                                    rx.button(
-                                        "Stiahnuť",
-                                        on_click=rx.download(url="/rozpis.txt"),
+                                rx.flex(
+                                    rx.dialog.close(
+                                        rx.button(
+                                            "Zatvoriť",
+                                            variant="soft",
+                                            color_scheme="gray",
+                                        ),
                                     ),
+                                    rx.dialog.close(
+                                        rx.button(
+                                            "Stiahnuť",
+                                            on_click=rx.download(url="/rozpis.txt"),
+                                        ),
+                                    ),
+                                    spacing="3",
+                                    justify="end",
                                 ),
-                                spacing="3",
-                                justify="end",
+                                max_width="fit-content",
                             ),
-                            max_width="fit-content",
-                        ),
-                    ),
-                    align_items="center",
-                ),
-                rx.upload(
-                    rx.vstack(
-                        rx.text("Nahrajte .txt súbor s infomáciami o časovom rozpise linky."),
-                        rx.button("Nahrať súbor"),
-                        rx.cond(
-                            stateClass.timeTableFilename != "",
-                            rx.text(stateClass.timeTableFilename),
-                            rx.text("\u00A0"),
                         ),
                         align_items="center",
                     ),
-                    id="upload_time_table",
-                    accept=".txt",
-                    max_files=1,
-                    padding="2em",
-                    multiple=False,
-                    width="100%",
-                    height="100%",
-                    on_drop=InfoUploadState.handle_upload_time_table(rx.upload_files("upload_time_table")),
+                    rx.upload(
+                        rx.vstack(
+                            rx.text("Nahrajte .txt súbor s infomáciami o časovom rozpise linky."),
+                            rx.button("Nahrať súbor"),
+                            rx.cond(
+                                stateClass.timeTableFilename != "",
+                                rx.text(stateClass.timeTableFilename),
+                                rx.text("\u00A0"),
+                            ),
+                            align_items="center",
+                        ),
+                        id="upload_time_table",
+                        accept=".txt",
+                        max_files=1,
+                        padding="2em",
+                        multiple=False,
+                        width="100%",
+                        height="100%",
+                        on_drop=InfoUploadState.handleUploadTimeTable(rx.upload_files("upload_time_table")),
+                    ),
+                    rx.center(
+                        rx.divider(),
+                        rx.text("alebo"),
+                        rx.divider(),
+                        width="100%",
+                        spacing="3",
+                    ),
+                    rx.select(
+                        InfoUploadState.dropdownOptions,  
+                        value=stateClass.selectedTimeTableName,
+                        on_change=InfoUploadState.selectTimeTableFromDropdown,
+                        size='3',
+                        width="100%",
+                    ),                 
+                    flex="1",
                 ),
-                rx.center(
-                    rx.divider(),
-                    rx.text("alebo"),
-                    rx.divider(),
-                    width="100%",
-                    spacing="3",
-                ),
-                rx.select(
-                    InfoUploadState.dropdownOptions,  
-                    value=stateClass.selectedTimeTableName,
-                    on_change=InfoUploadState.selectTimeTableFromDropdown,
-                    size='3',
-                    width="100%",
-                ),                 
-                flex="1",
             ),
             spacing="5",
             align_items="stretch",
             width="100%",
         ),
-
         rx.hstack(
             rx.vstack(
                 rx.cond(
@@ -225,23 +295,25 @@ def infoUpload(stateClass) -> rx.Component:
                     rx.text("\u00A0"),
                     
                 ),
-                align_items="end",
+                align_items=rx.cond(stateClass == AnalyzeLineState, "end", "center"),
                 flex="1",
             ),
-            rx.vstack(
-                rx.cond(
-                    stateClass.timeTable,
-                    rx.box(
-                        timeTable(stateClass.timeTable),
+            rx.cond(
+                stateClass == AnalyzeLineState,
+                rx.vstack(
+                    rx.cond(
+                        stateClass.timeTable,
+                        rx.box(
+                            timeTable(stateClass.timeTable),
+                        ),
+                        rx.text("\u00A0"),
                     ),
-                    rx.text("\u00A0"),
+                    flex="1",
                 ),
-                flex="1",
             ),
             spacing="5",
             width="100%",
         ),
-
         spacing="5",
         width="100%",
     )
