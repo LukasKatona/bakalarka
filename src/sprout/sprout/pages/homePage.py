e60d7d8086379b9ea8d2805629ad0e6aaa47a65f
Lukas Katona <katona.lukasek@gmail.com>
2025-05-05 14:46:55 +0200

    feat: homepage

--- src/sprout/sprout/pages/homePage.py
+++ src/sprout/sprout/pages/homePage.py
@@ -11,4 +11,67 @@ def homePage() -> rx.Component:
     :return: Home page component
     :rtype: rx.Component
     """
-    return rx.text("Welcome to SPROUT!!!"),
+    return rx.vstack(
+        rx.vstack(
+            rx.color_mode_cond(
+                light=rx.image(
+                    src="/logo-black.svg",
+                    width="10em",
+                    height="auto",
+                ),
+                dark=rx.image(
+                    src="/logo-white.svg",
+                    width="10em",
+                    height="auto",
+                ),
+            ),
+            rx.heading(
+                "SPROUT", size="9", weight="bold",
+            ),
+            rx.text(
+                "Smart Performance & Resource Optimization for Urban Transport",
+                size="5"
+            ),
+            align_items="center",
+            spacing="1",
+            width="100%",
+        ),
+        rx.vstack(
+            rx.text(
+                "Táto platforma poskytuje nástroje na analýzu a optimalizáciu cestovných poriadkov mestskej dopravy. "
+                "Umožňuje používateľom hodnotiť aktuálne cestovné poriadky, identifikovať neefektívnosti "
+                "a implementovať riešenia založené na dátach na zlepšenie prevádzkovej výkonnosti. "
+                "Cieľom je podporiť informované rozhodovanie a zefektívniť plánovanie dopravy v mestských oblastiach.",
+                align="center",
+            ),
+            rx.hstack(
+                rx.card(
+                    rx.text("Analýza linky", size="5", weight="bold"),
+                    rx.text(
+                        "Nahrajte a preskúmajte cestovné poriadky mestskej dopravy, aby ste získali prehľad o výkonnostných ukazovateľoch. "
+                        "Identifikujte slabé miesta a oblasti na zlepšenie pomocou tohto analytického nástroja."
+                    ),
+                    size="3",
+                    width="100%",
+                ),
+                rx.card(
+                    rx.text("Optimalizácia linky", size="5", weight="bold"),
+                    rx.text(
+                        "Preskúmajte optimalizované cestovné poriadky na zlepšenie efektívnosti. "
+                        "Využite pokročilé algoritmy na navrhovanie efektívnych plánov dopravy, "
+                        "ktoré zlepšujú prevádzkovú výkonnosť aj spokojnosť cestujúcich."
+                    ),
+                    size="3",
+                    width="100%",
+                ),
+                justify="between",
+                width="100%",
+            ),
+            align_items="center",
+            spacing="5",
+            width="75%",
+        ),
+        align_items="center",
+        padding_top="3em",
+        spacing="5",
+    )
